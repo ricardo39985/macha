c1c32fbe7871ea5b644c61ee3262af991771d8af
ricardo39985 <ricardo39985@gmail.com>
2022-10-20 23:06:40 -0400

    Set up

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {"id": 1, "name": "Learn Python"},
+    {"id": 2, "name": "Learn Ruby"},
+    {"id": 3, "name": "Learn JS"}
+
+
+]
+
+
+def home(request):
+    context = {"rooms":rooms}
+    return render(request, 'main/home.html', context)
+
+
+def room(request):
+    return render(request, 'main/room.html')
