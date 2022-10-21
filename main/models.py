ff0c58d2c2c19517c52efbd50e571c98ec9606f9
ricardo39985 <ricardo39985@gmail.com>
2022-10-20 23:33:48 -0400

    Add model room

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
+from unicodedata import name
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField()
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
